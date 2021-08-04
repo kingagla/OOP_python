80d0834a12fdbb811d542d2dcd202b1712be7db8
Kinga Głąbińska <kingagla@gmail.com>
2021-08-04 23:34:08 +0200

    correction and transaction info

--- Project_1/account.py
+++ Project_1/account.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import warnings
+from dateutil.tz import tzoffset
 
 
 class TransactionTypeError(Exception):
@@ -8,18 +9,21 @@ class TransactionTypeError(Exception):
 
 
 class BankAccount:
-    bank_tz = datetime.timezone.utc
+    transaction_number = 0
     interest_rate = 0.005
     acc_numbers = []
 
-    def __init__(self, account_number, first_name, last_name, initial_deposit=None):
+    def __init__(self, account_number, first_name, last_name, timezone=None, initial_deposit=None):
         if account_number in self.acc_numbers:
             raise ValueError("Account number must be UNIQUE! Please provide other number!")
         self._account_number = account_number
         self.acc_numbers.append(account_number)
         self._balance = 0
         self._set_owner(first_name, last_name)
-        self.acc_tz = datetime.timezone(datetime.timedelta(seconds=time.altzone * -1))
+        if isinstance(timezone, tzoffset):
+            self.acc_tz = timezone
+        else:
+            self.acc_tz = datetime.timezone(datetime.timedelta(seconds=time.altzone * -1))
         self.history = []
         if initial_deposit:
             self.transaction('deposit', initial_deposit)
@@ -46,7 +50,9 @@ class BankAccount:
         return self._balance
 
     def _generate_confirmation_number(self, transaction_type):
-        return f'{transaction_type}-{self.account_number}-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}'
+        self.transaction_number += 1
+        return f'{transaction_type}-{self.account_number}-\
+                    {datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d%H%M%S")}-{self.transaction_number}'
 
     def transaction(self, transaction_type, amount):
         if not (isinstance(amount, (float, int)) and amount > 0):
@@ -62,14 +68,21 @@ class BankAccount:
                 transaction_type = 'W'
         else:
             raise TransactionTypeError("Transaction type must be one of: ('deposit', 'withdrawal')")
-        return self._generate_confirmation_number(transaction_type)
+        confirmation = self._generate_confirmation_number(transaction_type)
+        self.history.append(confirmation)
+        return confirmation
 
     def interest_deposit(self):
         self.balance *= (1 + self.interest_rate)
-        return self._generate_confirmation_number('I')
+        confirmation = self._generate_confirmation_number('I')
+        self.history.append(confirmation)
+        return confirmation
 
     @staticmethod
     def transaction_info(transaction_id, timezone):
-        pass
-    # https://www.techatbloomberg.com/blog/work-dates-time-python/ douczyc sie o timezone i to co mam zmienic na taki
-    # timezone zeby wszystko wszedzie zwracalo jak nalezy
+        class Result:
+            transaction_type, account_number, utc_time, transaction_number = transaction_id.split('-')
+            account_number = int(account_number)
+            transaction_number = int(transaction_number)
+
+    # utc_time rozdzielic, zamienic na inty i zrobic z tego datetime.datetime() i ustawic timezone
