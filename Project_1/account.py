f4d523642aa7d9b31fa474a5b3cbf9d8d4dfe9b8
Kinga Głąbińska <kingagla@gmail.com>
2021-08-06 21:23:33 +0200

    last corrections

--- Project_1/account.py
+++ Project_1/account.py
@@ -10,19 +10,19 @@ class TransactionTypeError(Exception):
 class BankAccount:
     transaction_number = 0
     interest_rate = 0.005
-    acc_numbers = []
+    _acc_numbers = []
 
     def __init__(self, account_number, first_name, last_name, timezone=None, initial_deposit=None):
-        if account_number in self.acc_numbers:
+        if account_number in self._acc_numbers:
             raise ValueError("Account number must be UNIQUE! Please provide other number!")
         self._account_number = account_number
-        self.acc_numbers.append(account_number)
+        self._acc_numbers.append(account_number)
         self._balance = 0
-        self._set_owner(first_name, last_name)
+        self._set_owner((first_name, last_name))
         if isinstance(timezone, tzoffset):
-            self.acc_tz = timezone
+            self.timezone = timezone
         else:
-            self.acc_tz = datetime.timezone(datetime.timedelta(seconds=time.altzone * -1))
+            self.timezone = datetime.timezone(datetime.timedelta(seconds=time.altzone * -1))
         self.history = []
         if initial_deposit:
             self.transaction('deposit', initial_deposit)
@@ -51,19 +51,19 @@ class BankAccount:
     def _generate_confirmation_number(self, transaction_type):
         self.transaction_number += 1
         return f'{transaction_type}-{self.account_number}-\
-                    {datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d%H%M%S")}-{self.transaction_number}'
+{datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d%H%M%S")}-{self.transaction_number}'
 
     def transaction(self, transaction_type, amount):
         if not (isinstance(amount, (float, int)) and amount > 0):
             raise ValueError('Transaction amount must be positive value')
         if transaction_type == 'deposit':
-            self.balance += amount
+            self._balance += amount
             transaction_type = 'D'
         elif transaction_type == 'withdrawal':
             if self.balance < amount:
                 transaction_type = 'X'
             else:
-                self.balance -= amount
+                self._balance -= amount
                 transaction_type = 'W'
         else:
             raise TransactionTypeError("Transaction type must be one of: ('deposit', 'withdrawal')")
@@ -72,7 +72,7 @@ class BankAccount:
         return confirmation
 
     def interest_deposit(self):
-        self.balance *= (1 + self.interest_rate)
+        self._balance *= (1 + self.interest_rate)
         confirmation = self._generate_confirmation_number('I')
         self.history.append(confirmation)
         return confirmation
@@ -80,16 +80,13 @@ class BankAccount:
     @staticmethod
     def transaction_info(transaction_id, timezone):
         class TransactionInfo:
-            transaction_type, account_number, utc_time, transaction_number = transaction_id.split('-')
+            transaction_type, account_number, _date_utc, transaction_number = transaction_id.split('-')
             account_number = int(account_number)
             transaction_number = int(transaction_number)
-            date_utc = datetime.datetime.strptime(utc_time, "%Y%m%d%H%M%S")
-            transaction_datetime = date_utc + timezone.utcoffset(date_utc)
+            _date_utc = datetime.datetime.strptime(_date_utc, "%Y%m%d%H%M%S")
+            transaction_datetime = _date_utc + timezone.utcoffset(_date_utc)
             transaction_datetime = transaction_datetime.strftime('%Y-%m-%d %H:%M:%S')
 
         transaction_info = TransactionInfo()
         return transaction_info
 
-
-if __name__ == '__main__':
-    pass
